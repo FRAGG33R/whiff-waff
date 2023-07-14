c6186b361b5d35f5e4a5305b711df1b0ab10072b
Aissam Barchil <abarchil@e3r8p12.1337.ma>
2023-07-14 15:43:40 +0100

    chore : add the 3d Model
    add 3d modal that was build using blender to be rendred using
    react-three-fiber

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import "../app/globals.css";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home page
+    </div>
+  );
+}

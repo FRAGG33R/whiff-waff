df7d5611629456b5be646d474d2358cbf98947fd
FRAGG33R <aissambarchil75@gmail.com>
2023-07-15 17:25:37 +0100

    feat : make the zoom rotated to the screen angle

--- client/src/components/landingPage/room.tsx
+++ client/src/components/landingPage/room.tsx
@@ -4,9 +4,14 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
+import { useFrame } from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
+import { useRef, useState, WheelEvent } from "react";
 
-const RoomModel = () => {
+const RoomModel = (props: { deltaY: number }) => {
   const gltf = useLoader(GLTFLoader, "Model/Room3d.gltf");
+  const { deltaY } = props;
+  const { camera } = useThree();
 
   useEffect(() => {
     gltf.scene.scale.set(0.3, 0.3, 0.3);
@@ -27,18 +32,53 @@ const RoomModel = () => {
 };
 
 export default function Room() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [deltaY, setDeltaY] = useState(0);
+  const [rotation, setRotation] = useState(3);
+
+  const handleWheel = (e: WheelEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDeltaY(e.deltaY);
+
+    const rotateSpeed = 0.5;
+    const rotationDelta = e.deltaY * rotateSpeed * 0.001;
+    setRotation((prevRotation) => prevRotation + rotationDelta);
+  };
+  useEffect(() => {
+    console.log(rotation < 0 ? 0 : rotation * 3.66);
+	console.log(36 + (rotation < 0 ? rotation / 2 : rotation));
+	
+  }, [rotation]);
+
+  useEffect(() => {
+    containerRef.current?.addEventListener("wheel", handleWheel);
+    return () => {
+      containerRef.current?.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
   return (
-    <div className="flex w-full h-full items-center justify-center">
+    <div
+      ref={containerRef}
+      className="flex w-full h-full items-center justify-center"
+    >
       <Canvas style={{ width: "100%", height: "100%" }} shadows>
-        <PerspectiveCamera makeDefault position={[-9.9, 4, 11]} fov={40} />
+        <PerspectiveCamera
+          makeDefault
+          position={[-10.9, 4, rotation < 0 ? 0 : rotation * 3.66]}
+          fov={36 + (rotation < 0 ? rotation / 2 : rotation)}
+        />
         <color attach="background" args={["#E4E5E7"]} />
         <OrbitControls
+          enableZoom
           target={[0, 0.35, 0]}
           minAzimuthAngle={-Math.PI / 2}
           maxAzimuthAngle={Math.PI / 24}
           minPolarAngle={Math.PI / 6}
-          maxPolarAngle={Math.PI  / 2}
+          maxPolarAngle={Math.PI / 2}
+          rotateSpeed={0.5}
         />
+
         <spotLight intensity={0.45} position={[-20, 20, 20]} penumbra={0.2} />
         <spotLight
           position={[2, 13, 5]}
@@ -64,7 +104,8 @@ export default function Room() {
           intensity={2}
           penumbra={0.4}
         />
-        <RoomModel />
+
+        <RoomModel deltaY={deltaY} />
       </Canvas>
     </div>
   );

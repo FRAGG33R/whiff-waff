47c2b486ab074cce655471ba3f59cab2fb3f8d8b
FRAGG33R <aissambarchil75@gmail.com>
2023-07-18 16:51:19 +0100

    feat : implement progress bar

--- client/src/components/landingPage/room.tsx
+++ client/src/components/landingPage/room.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
@@ -16,8 +16,9 @@ import Image from "next/image";
 const RoomModel = (props: {
   rotation: number;
   setRotation: (rt: number) => void;
+  updateProgress: (loader: GLTFLoader) => void;
 }) => {
-  const gltf = useLoader(GLTFLoader, "Model/Room3d.gltf");
+  const gltf = useLoader(GLTFLoader, "Model/Room3d.gltf", props.updateProgress);
 
   const { rotation } = props;
   const { camera } = useThree();
@@ -68,6 +69,18 @@ export default function Room() {
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const router = useRouter();
 
+  const updateProgress = (loader: GLTFLoader) => {
+    loader.manager.onProgress = (url, itemsLoaded, itemsTotal) => {
+      const inputElement = document.getElementById(
+        "ProgressText"
+      ) as HTMLInputElement;
+      inputElement.textContent =
+        "[BUILDING " +
+        ((itemsLoaded / itemsTotal) * 100).toFixed(0).toString() +
+        "%]";
+    };
+  };
+
   const handleWheel = (e: WheelEvent<HTMLDivElement>) => {
     e.preventDefault();
     wheelDeltaBuffer.current.push(e.deltaY);
@@ -84,6 +97,7 @@ export default function Room() {
       return newRotation;
     });
   };
+
   useEffect(() => {
     rotation.toFixed(0) === "-11" ? router.push("/login") : null;
   }, [rotation]);
@@ -110,16 +124,20 @@ export default function Room() {
       >
         <div className="text-6xl md:text-7xl font-bold font-teko tracking-wider">
           WHIFF-WHAFF
+          <p
+            className="bg-transparent text-teko font-bold text-5xl flex items-center justify-center text-GreenishYellow"
+            id="ProgressText"
+          >
+            [BUILDING 0%]
+          </p>
         </div>
         <div className=" text-white md:w-full w-10/12 text-lg sm:text-2xl md:text-3xl  font-extralight text-center font-poppins">
           Futuristic ping pong at its finest. Unleash skills, challenge friends{" "}
         </div>
       </motion.div>
       <motion.div
-        whileInView={{ y: 0, transition: { duration: 1.9 } }}
-        style={{ y: 650 }}
         ref={containerRef}
-        className="flex w-full h-full items-center justify-center flex-col "
+        className="flex w-full h-full items-center justify-center flex-col"
       >
         <Canvas shadows className="md:flex hidden">
           <PerspectiveCamera
@@ -172,13 +190,17 @@ export default function Room() {
             intensity={2}
             penumbra={0.4}
           />
-          <RoomModel rotation={rotation} setRotation={setRotation} />
+          <RoomModel
+            rotation={rotation}
+            setRotation={setRotation}
+            updateProgress={updateProgress}
+          />
         </Canvas>
         <Image
           src="/room2.png"
           alt="room"
-		  width={600}
-		  height={600}
+          width={600}
+          height={600}
           className="bg-cover md:hidden "
         />
         <div className="absolute bottom-24 w-full h-12 flex items-center justify-center md:hidden">
@@ -189,6 +211,38 @@ export default function Room() {
             }}
           />
         </div>
+        <motion.div
+          style={{ y: 0, opacity: rotation > 0 ? Math.abs(rotation / 4) : 0 }}
+          className="absolute bottom-24 w-full h-12 md:flex items-center justify-center hidden flex-row  text-GreenishYellow "
+        >
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 1027 2112"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M949.774 1624.08L533.686 2089.74C524.387 2100.14 508.12 2100.2 498.749 2089.86L76.5478 1623.94C59.6754 1605.32 79.8644 1576.77 103.041 1586.48L505.987 1755.26C512.579 1758.02 520.007 1758 526.585 1755.21L923.063 1586.8C946.167 1576.98 966.499 1605.37 949.774 1624.08Z"
+              fill="#CBFC01"
+              stroke="#CBFC01"
+              stroke-width="3"
+            />
+            <path
+              d="M77.2264 487.369L493.314 21.7162C502.613 11.3092 518.88 11.2526 528.251 21.5945L950.452 487.516C967.325 506.136 947.136 534.679 923.959 524.972L521.013 356.195C514.421 353.434 506.993 353.453 500.415 356.247L103.937 524.657C80.8333 534.47 60.5008 506.087 77.2264 487.369Z"
+              fill="#CBFC01"
+              stroke="#CBFC01"
+              stroke-width="3"
+            />
+            <path
+              d="M849.146 1052.54C849.146 1239.91 699.034 1391.77 513.899 1391.77C328.763 1391.77 178.651 1239.91 178.651 1052.54C178.651 865.164 328.763 713.299 513.899 713.299C699.034 713.299 849.146 865.164 849.146 1052.54Z"
+              fill="#CBFC01"
+              stroke="#CBFC01"
+              stroke-width="3"
+            />
+          </svg>
+          <div className="font-teko font-bold text-3xl pt-1 ">SCROLL</div>
+        </motion.div>
       </motion.div>
     </div>
   );

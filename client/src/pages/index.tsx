b9b0b422e2e3116accba549cf259a20046e6b160
FRAGG33R <aissambarchil75@gmail.com>
2023-07-21 09:42:19 +0100

    feat : aniamte the cursor

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import "../app/globals.css";
 import Room from "@/components/landingPage/room";
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main className="flex md:min-h-screen h-screen items-start justify-center overflow-x-hidden bg-black overflow-y-hidden">
-		<Room />
+      <Room />
     </main>
   );
 }
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import "../app/globals.css";
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 export default function Login() {
   return (
-    <div className="flex md:min-h-screen h-screen items-center justify-center overflow-x-hidden  text-white bg-black">
-	  <div className="text-4xl">This is the log in page</div>
+    <div className="flex md:min-h-screen h-screen items-center justify-center   text-white bg-gradient-to-br from-DarkBg via-RhinoBlue to-ViolentViolet ">
+		<AnimatedCursor
+        color="203, 252, 1"
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+      />
     </div>
   );
 }

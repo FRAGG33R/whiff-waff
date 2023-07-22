76ccd07952839aad80ca6fa5e3565b85c15eba96
Houssam Lalouli <hlalouli@e3r9p13.1337.ma>
2023-07-22 09:52:31 +0100

    feat: finish Mute dropDown

--- client/src/components/ui/dropDowns/muteDropDown.tsx
+++ client/src/components/ui/dropDowns/muteDropDown.tsx
@@ -4,38 +4,51 @@ import {
     MenuHandler,
     MenuList,
     MenuItem,
-    Avatar,
     Typography,
   } from "@material-tailwind/react";
 import { IconBellOff } from '@tabler/icons-react';
-
+import { useState } from 'react';
+import {motion } from 'framer-motion';
 const MuteDropDown = () => {
+    const [selected, setSelected] = useState<string| null>(null);
+
+    const handleClick = (option:string) => {
+       setSelected(option);
+    };
+    
   return (
     <div>
-    <Menu>
-      <MenuHandler >
-        <IconBellOff />
-      </MenuHandler>
-      <MenuList className='bg-HokiCl border-0'>
-        <MenuItem className="flex items-centre justify-center gap-2">
-          <Typography  className="font-teko text-2xl text-Mercury">
-            1 hour
-          </Typography>
-        </MenuItem>
-        <MenuItem className="flex items-center justify-center gap-2">
-          <Typography  className="font-teko text-2xl text-Mercury">
-            8 hours
-          </Typography>
-        </MenuItem>
-        <MenuItem className="flex items-center justify-center gap-2">   
-          <Typography  className="font-teko text-2xl text-Mercury">
-            Always
-          </Typography>
-        </MenuItem>
-      </MenuList>
-    </Menu>
+        <Menu>
+        <MenuHandler >
+        <motion.div
+            initial={{ scale: 0.9 }}
+            whileHover={{ scale: 1 }}
+         >
+            <IconBellOff />
+        </motion.div>
+        </MenuHandler>
+        <MenuList className='bg-HokiCl border-0 '>
+            <MenuItem className="flex items-centre justify-center gap-2 "
+                onClick={() => handleClick("1 hour")}>
+            <Typography  className="font-teko text-2xl text-Mercury ">
+                1 hour
+            </Typography>
+            </MenuItem>
+            <MenuItem className="flex items-center justify-center gap-2 "
+                    onClick={()=> handleClick("8 hour")} >
+            <Typography  className="font-teko text-2xl text-Mercury">
+                8 hours
+            </Typography>
+            </MenuItem>
+            <MenuItem className="flex items-center justify-center gap-2"
+                        onClick={()=> handleClick("always")}>   
+            <Typography  className="font-teko text-2xl text-Mercury">
+                Always
+            </Typography>
+            </MenuItem>
+        </MenuList>
+        </Menu>
     </div>
-  )
+  );
 }
-
 export default MuteDropDown

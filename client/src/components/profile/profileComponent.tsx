19ae358d57d3b16368ca60570ad3958e2aafc479
FRAGG33R <aissambarchil75@gmail.com>
2023-07-24 14:22:52 +0100

    feat : profile architect

--- client/src/components/profile/profileComponent.tsx
+++ client/src/components/profile/profileComponent.tsx
@@ -1,16 +1,28 @@
-
 export default function Profile() {
-	return (
-		<div className="w-[98%] h-[95%] flex items-center justify-start flex-row bg-black/60 J">
-			<div className="h-full w-[20%] md:w-[7%] border-2 border-white"></div>
-			<div className="h-full w-full border-2 border-white">
-				<div className="h-[7%] w-full border-2 border-white bg-red-100"></div>
-				<div className="h-[93%] w-full border-2 border-white bg-green-100">
-					<div className="w-full h-[40%] border-2 border-white"></div>
-					<div className="w-full h-[60%] border-2 border-white"></div>
-				</div>
-			</div>
-		</div>
-		
-	)
+  return (
+    <div className="w-full md:w-[98%] h-full md:h-[95%] flex items-center justify-start flex-row bg-black/60 text-white">
+      <div className="h-full w-[13%] lg:w-[5%] border-2 border-white"></div>
+      <div className="h-full w-full border-2 border-white">
+        <div className="h-[7%] w-full border-2 border-white"></div>
+        <div className="h-[93%] w-full border-2 border-white overflow-auto lg:overflow-hidden">
+          <div className="w-full h-[60%] lg:h-[35%] flex flex-col lg:flex-row border-2 border-white">
+            <div className="w-full lg:w-[55%] h-full border-2 border-white">
+              profile infos
+            </div>
+            <div className="w-full lg:w-[45%] h-full border-2 border-white">
+              rank
+            </div>
+          </div>
+          <div className="w-full h-full flex flex-col-reverse lg:flex-row border-2 border-white">
+            <div className="w-full h-[50%] lg:h-full border-2 border-white">
+              match history
+            </div>
+            <div className="w-full lg:w-7/12  h-[50%] lg:h-full border-2 border-white">
+              achievement
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

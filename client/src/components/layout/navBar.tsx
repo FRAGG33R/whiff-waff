49c0830f7adc3f8973b973ec528805a3e75f51ff
FRAGG33R <aissambarchil75@gmail.com>
2023-07-25 20:11:53 +0100

    feat : add search component in navbar

--- client/src/components/layout/navBar.tsx
+++ client/src/components/layout/navBar.tsx
@@ -1,7 +1,14 @@
+import SearchInput from "../ui/inputs/searchInput"
 
 export default function NavBar() {
 	return (
-		<div className="w-full h-full bg-green-100">
+		<div className="w-full h-full flex flex-row space-x-6 lg:space-x-0 lg:justify-between bg-[#606060]/[12%] rounded-[20px]">
+			<div className="h-full w-[40%] lg:w-[46%] xl:w-[40%] flex flex-row items-center">
+				<SearchInput onSearch={() =>{}} placeholder="Search for everything..."/>
+			</div>
+			<div className="h-full w-[15%] lg:w-[12%] xl:w-[10%] flex flex-row border-2 border-blue-300"></div>
+			<div className="h-full w-[15%] lg:w-[12%] xl:w-[10%] flex flex-row border-2 border-blue-300"></div>
+			<div className="h-full w-[30%] lg:w-[20%] xl:w-[15%] flex flex-row border-2 border-blue-300"></div>
 		</div>
 	)
 }
--- client/src/components/ui/inputs/searchInput.tsx
+++ client/src/components/ui/inputs/searchInput.tsx
@@ -3,22 +3,22 @@ import Image from 'next/image';
 import VectorIcon from '../../../../public/searchIcon.svg'
 import { searchInputProps } from '../../../types/inputsType';
 
-const searchInput: React.FC < searchInputProps> = ({onSearch}) => {
+const SearchInput: React.FC < searchInputProps> = ({onSearch}) => {
   
     return (
-      <div className='relative w-full text-gray-600'>
+      <div className='relative w-full h-full text-gray-600 bg-transparent'>
         <input 
             type={'search'}
             name={'search'}
             placeholder={'Search for everything...'}
             onChange={e => onSearch(e.target.value)}
-            className='bg-CarbonGrey bg-opacity-20 w-64 sm:w-28 :md:w-40 lg:w-96 h-10 md:h-9 lg:h-10 px-10 pr-5  rounded-full text-sm focus:outline-none'
+            className='bg-transparent w-full placeholder:text-[#6C7FA7] placeholder:font-poppins h-full pl-16 text-xl focus:outline-none'
         />
-        <button type='submit' className='absolute left-0 top-0 mt-3 ml-4'>
-            <Image src={VectorIcon } alt='vector icon' width={15} />
+        <button type='submit' className='w-8 h-full absolute left-5'>
+            <Image src={VectorIcon } alt='vector icon'/>
         </button>
       </div>
     );
 }
 
-export default searchInput
+export default SearchInput

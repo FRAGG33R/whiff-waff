bf5e0dd36e66e658b2da6a94825e3fc7ea8d20fb
FRAGG33R <aissambarchil75@gmail.com>
2023-07-25 17:48:50 +0100

    feat : add sidebar icons

--- client/src/components/layout/sideBar.tsx
+++ client/src/components/layout/sideBar.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image"
+import GreenishLogo from '../../../public/greenishLogo.svg'
 
 export default function SideBar() {
 	return (
-		<div className="w-full h-full bg-blue-gray-600">
-
+		<div className="w-full h-full bg-[#606060]/[12%] rounded-[20px]">
+			<div className="h-[10%] w-full flex items-center justify-center bg-cover">
+				<Image src={GreenishLogo} alt="Greenish Logo" />
+			</div>
+			<div className="h-[90%] w-full flex flex-col">
+				
+			</div>
 		</div>
 	)
 }

0815e60d62a1f0311a5b4127da553b1381c97de7
kmlFaouzi <faouzikamal27@gmail.com>
2023-08-03 14:48:27 +0100

    REFACTOR: structering the dto file

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,26 +1,31 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Exclude } from "class-transformer";
-import { IsAlpha, IsBoolean, IsEmail, IsEnum, IsNotEmpty, Matches, MaxLength, MinLength, isBoolean, isEmail } from "class-validator";
+import { IsEmail, IsNotEmpty, Matches, MinLength } from "class-validator";
 import * as values from 'src/shared/constants/constants.values'
 export class SignUpDto {
 
+    @Matches(/^.{3,}/)
     @ApiProperty()
     @IsNotEmpty()
     userName: string;
 
+    @Matches(/^.{3,}/)
     @ApiProperty()
     @IsNotEmpty()
     firstName: string;
 
+    @Matches(/^.{3,}/)
     @ApiProperty()
     @IsNotEmpty()
     lastName: string;
 
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
     @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @Matches(/^.{6,}$/)
     @ApiProperty()
     @MinLength(6)
     @IsNotEmpty()

3fb78e086761d24286edea9bb4496b382f64055a
houssamLalouli <lalouli.houssam2017@gmail.com>
2023-09-01 21:09:50 +0100

    fixe : small update

--- client/src/components/game/gamePage.tsx
+++ client/src/components/game/gamePage.tsx
@@ -38,11 +38,11 @@ const GamePage: React.FC = () => {
   console.log(divRef.current);
   return (
     <div className="w-[98%] h-[96%] md:h-[95%] flex items-center justify-start gap-2 md:gap-10 flex-row overflow-y-hidden">
-      <div className="h-full border  min-w-[40px] w-[30px] md:w-[100px]">
+      <div className="h-full   min-w-[40px] w-[30px] md:w-[100px]">
       <SideBar />
       </div>
       <div className="h-full  w-[82%] md:w-[90%] xl:w-[95%] space-y-2 md:space-y-8 ">
-        <div className="h-[45px] border  md:h-[50px] lg:h-[60px] xl:h-[70px] w-full">
+        <div className="h-[45px]   md:h-[50px] lg:h-[60px] xl:h-[70px] w-full">
         <NavBar level={String((userData as userType).stat.level)} avatar={(userData as userType).avatar} useName={(userData as userType).userName}/>
         </div>
         <div className="w-[99%] md:h-[93%] sm:h-[95%] lg:h-[91.5%] h-[96.5%] overflow-y-scroll xl:overflow-y-auto space-y-2 xl:space-y-10 flex flex-col items-start justify-start  ">

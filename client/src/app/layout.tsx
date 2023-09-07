0c81b15b34aa83486a8e833e2e27ff19d1db3901
FRAGG33R <aissambarchil75@gmail.com>
2023-09-07 11:08:59 +0100

    feat : before testing and refactoring everything

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-
         <body className={inter.className}>{children}</body>
     </html>
   );
--- client/src/components/chat/conversation.tsx
+++ client/src/components/chat/conversation.tsx
@@ -1,45 +1,70 @@
 export default function Conversation() {
   const conversation = [
     {
+      type: "sender",
       content:
         "Lorem Ipsum dolar sign nisi an abstruction of the middle, lines are broken !",
       time: "2:33",
-      user: "Navoos",
+      userName: "Navoos",
     },
     {
+      type: "receiver",
       content:
         "Lorem Ipsum dolar sign nisi an abstruction of the middle, lines are broken !",
       time: "4:13",
-      user: "Navoos",
+      userName: "Navoos",
     },
     {
+      type: "sender",
       content:
         "Lorem Ipsum dolar sign nisi an abstruction of the middle, lines are broken !",
       time: "3:12",
-      user: "Navoos",
+      userName: "Navoos",
+    },
+    {
+      type: "receiver",
+      content:
+        "Lorem Ipsum dolar sign nisi an abstruction of the middle, lines are broken !",
+      time: "3:12",
+      userName: "Navoos",
+    },
+    {
+      type: "sernder",
+      content:
+        "Lorem Ipsum dolar sign nisi an abstruction of the middle, lines are broken !",
+      time: "3:12",
+      userName: "Navoos",
     },
   ];
 
   return (
     <div className="w-full h-full">
-      <div className="chat chat-start text-black">
-        <div className="bg-GreenishYellow px-4 rounded-[12px] py-2 flex flex-col space-y-6 items-center justify-center">
-		  <div className="w-full flex flex-row items-center justify-between font-normal font-teko text-xl md:text-2xl">
-			<div>{conversation[0].user}</div>
-			<div>{conversation[0].time}</div>
-		  </div>
-		  <div className="font-poppins">It's over Anakin, <br />I have the high ground.</div>
-        </div>
-      </div>
-      <div className="chat chat-end text-white">
-        <div className="bg-HokiCl px-4 rounded-[12px] py-2 flex flex-col space-y-6 items-center justify-center">
-		  <div className="w-full flex flex-row items-center justify-between font-normal font-teko text-xl md:text-2xl">
-			<div>{conversation[0].user}</div>
-			<div>{conversation[0].time}</div>
-		  </div>
-		  <div className="font-poppins">It's over Anakin, <br />I have the high ground.</div>
-        </div>
-      </div>
+      {conversation.map((item, index) => {
+        return (
+          <div
+			key={index}
+            className={`chat ${
+              item.type === "receiver"
+                ? "chat-end text-white"
+                : "chat-start text-black"
+            } w-full `}
+          >
+            <div
+              className={`${
+                item.type === "receiver" ? "bg-HokiCl" : "bg-GreenishYellow"
+              } px-4 md:w-[200px] lg:w-[300px] xl:w-[400px] 2xl:w-[600px] rounded-[12px] py-4 flex flex-col space-y-6 items-start justify-center`}
+            >
+              <div className="w-full flex flex-row items-center justify-between font-normal font-teko text-xl md:text-2xl">
+                <div>{item.userName}</div>
+                <div>{item.time}</div>
+              </div>
+              <div className="font-poppins w-full ">
+                It's over Anakin, I have the high ground.
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }

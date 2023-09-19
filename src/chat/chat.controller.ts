fabe9210d3899639d5fcaf2e0f5b7c5c19b18280
kmlFaouzi <faouzikamal27@gmail.com>
2023-09-19 23:12:19 +0100

    REFACTOR: add return types to chat methods

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -3,6 +3,7 @@ import { ConversationDto, RoomInfos } from 'src/dto/chat.dto';
 import { ChatService } from './chat.service';
 import { JwtGuard } from 'src/auth/guards/guards.jwtGuard';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { AllUserConversationsResponse, IndividualConversationResponse } from 'src/custom_types/custom_types.Individual-chat';
 
 const chatController = 'chat'
 const conversationEndpoint = 'individualConversations'
@@ -15,26 +16,27 @@ export class ChatController {
 	@ApiBearerAuth()
 	@UseGuards(JwtGuard)
 	@Get(conversationEndpoint)
-	async getConversations(@Query() data: ConversationDto, @Req() req: Request) {
+	async getConversations(@Query() data: ConversationDto, @Req() req: Request): Promise<AllUserConversationsResponse> {
 		data.nbElements = (!data.nbElements) ? data.nbElements = undefined : Number(data.nbElements);
 		data.nbPage = (!data.nbPage) ? data.nbPage = undefined : Number(data.nbPage);
 		const loggedUserId = (req as any).user.id;
 		const loggedUser: any = { avatar: (req as any).user.avatar, userName: (req as any).user.userName, level: (req as any).user.stat.level };
 		const allConversation = await this.chatService.getAllConversationsById(loggedUserId, data);
-		return { allConversation, loggedUser };
+		return { allConversation, loggedUser } as any;
 	}
 
 	@ApiBearerAuth()
 	@UseGuards(JwtGuard)
 	@Get(individualConversation)
-	async getIndividualConversation(@Query() data: ConversationDto, @Req() req: Request) {
+	async getIndividualConversation(@Query() data: ConversationDto, @Req() req: Request): Promise<IndividualConversationResponse> {
 		data.nbElements = (!data.nbElements) ? data.nbElements = undefined : Number(data.nbElements);
 		data.nbPage = (!data.nbPage) ? data.nbPage = undefined : Number(data.nbPage);
 		const loggedUserId = (req as any).user.id;
 		const receivedId = (req as any).params.receiverId;
 		return await this.chatService.getIndividualConversationById(loggedUserId, receivedId, data);
 	}
-
+	
+	@ApiBearerAuth()
 	@UseGuards(JwtGuard)
 	@Post(createRoom)
 	async joinRoom(@Body() data: RoomInfos, @Req() req: Request) {
